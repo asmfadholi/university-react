7f4ed088cd818f0f59ec7b9ebed5a793c4bdd7db
Mochammad Fadholi <mochammad.fadholi.st@gmail.com>
2020-07-22 02:06:33 +0700

    fix: destructuring props in input search

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -2,18 +2,23 @@ import React from 'react';
 import { MdSearch } from 'react-icons/md';
 import { Form, Input } from 'reactstrap';
 
-const SearchInput = (props) => (
-  <Form inline className="cr-search-form" onSubmit={(e) => e.preventDefault()}>
-    <MdSearch
-      size="20"
-      className="cr-search-form__icon-search text-secondary"
-    />
-    <Input
-      type="search"
-      className="cr-search-form__input"
-      placeholder={props.placeholder}
-    />
-  </Form>
-);
+const SearchInput = (props) => {
+  const { placeholder } = props;
+  return (
+
+    <Form inline className="cr-search-form" onSubmit={(e) => e.preventDefault()}>
+      <MdSearch
+        size="20"
+        className="cr-search-form__icon-search text-secondary"
+      />
+      <Input
+        type="search"
+        className="cr-search-form__input"
+        placeholder={placeholder}
+      />
+    </Form>
+
+  );
+};
 
 export default SearchInput;
